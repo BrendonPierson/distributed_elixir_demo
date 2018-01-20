84b43d80425ce2122b055c7cc94100840f8e1aca
Brendon Pierson <BrendonPierson@gmail.com>
2018-01-20 15:32:31 -0600

    Created GenServer to demo node monitoring

--- lib/dist_demo.ex
+++ lib/dist_demo.ex
@@ -1,18 +1,36 @@
 defmodule DD do
   @moduledoc """
-  Documentation for DD.
+  Simple GenServer to demonstrate monitoring elixir nodes
   """
+  use GenServer
+
+  def start_link, do: GenServer.start_link(__MODULE__, [], name: __MODULE__)
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> DD.hello
-      :world
-
+  Start monitoring all the nodes in the cluster.
   """
-  def hello do
-    :world
+  def monitor_nodes, do: GenServer.call(__MODULE__, :monitor_cluster)
+
+  def init(_), do: {:ok, []}
+
+  def handle_call(:monitor_cluster, _, state) do
+    :net_kernel.monitor_nodes(true)
+    {:reply, :ok, state}
   end
+
+  def handle_info({:nodedown, node}, state) do
+    IO.puts("Node has gone down: #{node}")
+    {:noreply, state}
+  end
+
+  def handle_info({:nodeup, node}, state) do
+    IO.puts("Node is up: #{node}")
+    {:noreply, state}
+  end
+
+  def handle_info(msg, state) do
+    IO.inspect msg
+    {:noreply, state}
+  end
+
 end
